f1fdaf69e925b47789440bbffa1936cddcc316db
20191844339 <56439219+20191844339@users.noreply.github.com>
2019-10-22 00:27:43 -0500

    Add files via upload

---
+++ cly3 1/cly3 1.cpp
@@ -0,0 +1,12 @@
+// cly3 1.cpp : Defines the entry poi#include<stdio.h>
+ #include<stdio.h>
+#include<math.h>
+int  main(){
+	int  i=10;
+	double r=0.07,p;
+	p=pow(1+r,10);
+	printf("十年后与现在相比的倍数%lf",p);
+	return 0;
+	
+}
+
