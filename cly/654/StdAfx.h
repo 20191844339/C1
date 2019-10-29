4dc2ff365797fe6567432b3b7f852ccf8c412a15
20191844339 <56439219+20191844339@users.noreply.github.com>
2019-10-29 08:25:57 +0800

    Add files via upload

---
+++ cly/654/StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__3E676446_9D16_4FFC_AEEE_CB4BCBCB3445__INCLUDED_)
+#define AFX_STDAFX_H__3E676446_9D16_4FFC_AEEE_CB4BCBCB3445__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__3E676446_9D16_4FFC_AEEE_CB4BCBCB3445__INCLUDED_)
